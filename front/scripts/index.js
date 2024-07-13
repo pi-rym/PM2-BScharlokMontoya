a83c4ddb2a2f6c60336183e34f77b30f822e491e
Brahyam Scharlok <brahyammontoya@hotmail.com>
2024-07-13 16:37:28 -0500

    clase 1 incompleta

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,15 @@
+const tempData = require("./tempData.js");
+
+class Movie {
+  constructor(title, year, director, duration, genre, rate, poster) {
+    this.title;
+    this.year;
+    this.director;
+    this.duration;
+    this.genre;
+    this.rate;
+    this.poster;
+  }
+}
+
 console.log(tempData);
--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -30,3 +30,5 @@ const tempData = [
       "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
   },
 ];
+
+module.exports = { tempData };
