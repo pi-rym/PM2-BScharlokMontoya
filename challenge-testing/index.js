80c062970ac46f0d4b9767a62037247124485dea
Brahyam Scharlok <brahyammontoya@hotmail.com>
2024-07-30 21:45:40 -0500

    listo

--- challenge-testing/index.js
+++ challenge-testing/index.js
@@ -1,21 +1,21 @@
-class CarritoCompra{
-    constructor(){
-        this.products = []
+class CarritoCompra {
+  constructor() {
+    this.products = [];
+  }
+  agregarProducto(product) {
+    this.products.push(product);
+  }
+  calcularTotal() {
+    let total = 0;
+    for (const item of this.products) {
+      total += item.price * item.quantity;
     }
-    agregarProducto(product){
-this.products.push(product)
-    }
-    calcularTotal(){
-        let total = 0;
-        for(const item of this.products){
-            total += item.price * item.quantity
-        }
-        return total
-    }
-    aplicarDescuento(porcentaje){
-        const subTotal =this.calcularTotal()
-        const descuento = this.calcularTotal() * (porcentaje/100)
-        return subTotal-descuento
-    }
-};
+    return total;
+  }
+  aplicarDescuento(porcentaje) {
+    const subTotal = this.calcularTotal();
+    const descuento = this.calcularTotal() * (porcentaje / 100);
+    return subTotal - descuento;
+  }
+}
 module.exports = CarritoCompra;
